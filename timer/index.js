c13d427d1ca2d65b3999e8fda2de3ff1caac0084
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2023-05-24 18:55:04 -0500

    timer complete with improvements

--- timer/index.js
+++ timer/index.js
@@ -1,48 +1,40 @@
-class Timer{
-    constructor(durationInput,startButton, pauseButton){
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click',this.start);
-        this.pauseButton.addEventListener('click', this.pause)
-    }
-
-    start =()=>{
-        this.tick();
-        this.interval = setInterval(this.tick, 1000)
-        console.log(timer)
-    }
-
-    pause = () =>{
-        clearInterval(this.interval)
-    }
-
-    tick = ()=>{
-        if(this.timeRemaining <= 0){
-            this.pause();
-        }
-        else{
-            this.timeRemaining = this.timeRemaining - 1;
-        }
-    }
-
-    get timeRemaining(){
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time){
-        this.durationInput.value=time;
-    }
-
-
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+function getColor(percentComplete) {
+    if (percentComplete <= 0.1) {
+      return 'red';
+    } else if (percentComplete <= 0.25) {
+      return 'orange';
+    } else if (percentComplete <= 0.5) {
+      return 'yellow';
+    } else {
+      return 'green';
+    }
+  }
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration;
+        console.log('Started');
+    },
+    onTick(timeRemaining) {
+        let percentComplete = timeRemaining / duration;
+        let movingPerimeter = perimeter * percentComplete - perimeter;
+        circle.setAttribute('stroke-dashoffset', movingPerimeter);
+        let color = getColor(percentComplete);
+        circle.setAttribute('stroke', color);
+    },
+    onComplete() {
+        console.log('Completed');
+    }
+});
 
 
 
